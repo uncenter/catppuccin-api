c41a03068ce4a1866d5722d44d6aac97aac74c37
uncenter <47499684+uncenter@users.noreply.github.com>
2024-11-15 10:38:17 -0500

    refactor: change port to 8080

--- src/main.rs
+++ src/main.rs
@@ -64,7 +64,7 @@ async fn main() {
         .route("/showcases", get(list_showcases));
 
     // run our app with hyper, listening globally on port 3000
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
